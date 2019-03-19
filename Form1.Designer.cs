b147ad7a4e172acf1f0832e8978a013c9baabda8
vaside <bart.rogovsky@gmail.com>
2019-03-19 22:17:27 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace _2048_Application
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(31)))), ((int)(((byte)(31)))));
+            this.label2.Location = new System.Drawing.Point(0, -2);
+            this.label2.Margin = new System.Windows.Forms.Padding(0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(616, 35);
+            this.label2.TabIndex = 1;
+            this.label2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label2_MouseDown);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(185)))), ((int)(((byte)(173)))), ((int)(((byte)(159)))));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(557, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(54, 22);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseLeave += new System.EventHandler(this.pictureBox1_MouseLeave);
+            this.pictureBox1.MouseHover += new System.EventHandler(this.pictureBox1_MouseHover);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(185)))), ((int)(((byte)(173)))), ((int)(((byte)(159)))));
+            this.ClientSize = new System.Drawing.Size(615, 687);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "2048";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,406 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Windows.Input;
+using System.Drawing.Text;
+
+namespace _2048_Application
+{
+    public partial class Form1 : Form
+    {
+        protected IDictionary<int, string> InitColorIDictionary = new Dictionary<int, string>();
+        protected void InitColorList()
+        {
+            InitColorIDictionary[0] = "#CCC0B3";
+            InitColorIDictionary[2] = "#EEE4DA";
+            InitColorIDictionary[4] = "#EDE0C8";
+            InitColorIDictionary[8] = "#F2B179";
+            InitColorIDictionary[16] = "#F59563";
+            InitColorIDictionary[32] = "#F67C5F";
+            InitColorIDictionary[64] = "#F65E3B";
+            InitColorIDictionary[128] = "#EDCF72";
+            InitColorIDictionary[256] = "#EDCC61";
+            InitColorIDictionary[512] = "#EDC850";
+            InitColorIDictionary[1024] = "#EDC53F";
+            InitColorIDictionary[2048] = "#EDC22E";
+            InitColorIDictionary[4096] = "#3E3933";
+        }
+        protected IDictionary<int, string> FontColorIDictionary = new Dictionary<int, string>();
+        protected void FontColorList()
+        {
+            FontColorIDictionary[0] = "#776E65";
+            FontColorIDictionary[2] = "#776E65";
+            FontColorIDictionary[4] = "#776E65";
+            FontColorIDictionary[8] = "#F9F6F2";
+            FontColorIDictionary[16] = "#F9F6F2";
+            FontColorIDictionary[32] = "#F9F6F2";
+            FontColorIDictionary[64] = "#F9F6F2";
+            FontColorIDictionary[128] = "#F9F6F2";
+            FontColorIDictionary[256] = "#F9F6F2";
+            FontColorIDictionary[512] = "#F9F6F2";
+            FontColorIDictionary[1024] = "#F9F6F2";
+            FontColorIDictionary[2048] = "#F9F6F2";
+            FontColorIDictionary[4096] = "#F9F6F2";
+        }
+        protected Label[,] labels = new Label[4, 4];
+        protected int[,] Board = new int[4, 4] { { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 } };
+
+        protected void GenerateBoard()
+        {
+            PrivateFontCollection pfc = new PrivateFontCollection();
+            pfc.AddFontFile("ClearSans-Bold.ttf");
+            int x_box = 30;
+            int y_box = 90;
+            for (int j = 0; j <= 3; j++)
+            {
+                for (int i = 0; i <= 3; i++)
+                {
+                    Label label = new Label();
+                    label.AutoSize = false;
+                    label.Size = new System.Drawing.Size(125, 125);
+                    label.BackColor = System.Drawing.ColorTranslator.FromHtml("#CCC0B2");
+                    label.Location = new System.Drawing.Point(x_box, y_box);
+                    label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+                    label.Font = new System.Drawing.Font(pfc.Families[0], 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+                    label.ForeColor = System.Drawing.Color.White;
+                    label.Text = "";
+                    label.Show();
+                    Controls.Add(label);
+                    labels[j, i] = label;
+                    x_box += 145;
+
+                }
+                x_box = 30;
+                y_box += 145;
+            }
+        }
+        protected void RefreshStatus()
+        {
+            for (int y = 0; y <= 3; y++)
+            {
+                for (int x = 0; x <= 3; x++)
+                {
+                    if(Board[y, x] == 0)
+                    {
+                        labels[y, x].Text = "";
+                        labels[y, x].BackColor = System.Drawing.ColorTranslator.FromHtml(InitColorIDictionary[0]);
+                    }
+                    else
+                    {
+                        labels[y, x].Text = Board[y, x].ToString();
+                        labels[y, x].BackColor = System.Drawing.ColorTranslator.FromHtml(InitColorIDictionary[Board[y, x]]);
+                        if (Board[y, x] == 2 || Board[y, x] == 4)
+                        {
+                            labels[y, x].ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(119)))), ((int)(((byte)(110)))), ((int)(((byte)(101)))));
+                        }
+                        else
+                        {
+                            labels[y, x].ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(246)))), ((int)(((byte)(242)))));
+                        }
+                    }      
+                }
+            }
+        }
+        private void ReturnArray()
+        {
+            for (int y = 0; y <= 3; y++)
+            {
+                for (int x = 0; x <= 3; x++)
+                {
+                    Console.Write("{0}, ", Board[y, x]);
+                }
+                Console.Write("\n\n");
+            }
+            Console.Write("\n\n");
+        }
+        private void FirstPoint()
+        {
+            Random n = new Random();
+            var i = n.Next(0, 4);
+            var j = n.Next(0, 4);
+            Board[j, i] = 2;
+        }
+        private void ShuffleNumber()
+        {
+            bool ShuffleStatus = true;
+            bool GeneralStatus = false;
+            for(int i = 0; i <= 3; i++)
+            {
+                for(int j = 0; j <= 3; j++)
+                {
+                    if(Board[i, j] == 0)
+                    {
+                        GeneralStatus = true;
+                    }
+                }
+            }
+            if(GeneralStatus == true)
+            {
+                while (ShuffleStatus)
+                {
+                    Random t = new Random();
+                    var y = t.Next(0, 4);
+                    var x = t.Next(0, 4);
+                    if (Board[y, x] == 0)
+                    {
+                        Board[y, x] = 2;
+                        ShuffleStatus = false;
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Koniec gry", "Koniec gry", MessageBoxButtons.OKCancel);
+            }
+        }
+        private int[] BLOCK_MOVE = new int[4] { 0, 0, 0, 0 };
+        protected void mLeft()
+        {
+            for (int y = 0; y <= 3; y++)
+            {
+                for(int x = 1; x <= 3; x++)
+                {
+                    if(Board[y, x] != 0)
+                    {
+                        for(int z = x - 1; z >= 0; z--)
+                        {
+                            if (Board[y, z] == 0)
+                            {
+                                Board[y, z] = Board[y, z + 1];
+                                Board[y, z + 1] = 0;
+                            }
+                            else if (Board[y, z] != 0 && Board[y, z] != Board[y, z + 1])
+                            {
+
+                            }
+                            else if (Board[y, z] == Board[y, z + 1])
+                            {
+                                Board[y, z] = Board[y, z + 1] * 2;
+                                Board[y, z + 1] = 0;
+                            }
+                        }
+                    }
+                }
+            }
+        }  
+        protected void mUp()
+        {
+            for (int y = 1; y <= 3; y++)
+            {
+                for (int x = 0; x <= 3; x++)
+                {
+                    if (Board[y, x] != 0)
+                    {
+                        for (int z = y - 1; z >= 0; z--)
+                        {
+                            if (Board[z, x] == 0)
+                            {
+                                Board[z, x] = Board[z + 1, x];
+                                Board[z + 1, x] = 0;
+                            }
+                            else if (Board[z, x] != 0 && Board[z, x] != Board[z + 1, x])
+                            {
+
+                            }
+                            else if (Board[z, x] == Board[z + 1, x])
+                            {
+                                Board[z, x] = Board[z + 1, x] * 2;
+                                Board[z + 1, x] = 0;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        protected void mRight()
+        {
+            for (int y = 0; y <= 3; y++)
+            {
+                for (int x = 2; x >= 0; x--)
+                {
+                    if (Board[y, x] != 0)
+                    {
+                        for (int z = x + 1; z <= 3; z++)
+                        {
+                            if (Board[y, z] == 0)
+                            {
+                                Board[y, z] = Board[y, z - 1];
+                                Board[y, z - 1] = 0;
+                            }
+                            else if (Board[y, z] != 0 && Board[y, z] != Board[y, z - 1])
+                            {
+
+                            }
+                            else if (Board[y, z] == Board[y, z - 1])
+                            {
+                                Board[y, z] = Board[y, z - 1] * 2;
+                                Board[y, z - 1] = 0;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        protected void mDown()
+        {
+            for (int y = 2; y >= 0; y--)
+            {
+                for (int x = 0; x <= 3; x++)
+                {
+                    if (Board[y, x] != 0)
+                    {
+                        for (int z = y + 1; z <= 3; z++)
+                        {
+                            if (Board[z, x] == 0)
+                            {
+                                Board[z, x] = Board[z - 1, x];
+                                Board[z - 1, x] = 0;
+                            }
+                            else if (Board[z, x] != 0 && Board[z, x] != Board[z - 1, x])
+                            {
+
+                            }
+                            else if (Board[z, x] == Board[z - 1, x])
+                            {
+                                Board[z, x] = Board[z - 1, x] * 2;
+                                Board[z - 1, x] = 0;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        private List<string> pressedList = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+            InitColorList();
+            GenerateBoard();
+            FirstPoint();
+            RefreshStatus();
+            
+            pressedList.Add(Keys.Space.ToString());
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+        
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {    
+            string pressedKey = e.KeyData.ToString();
+            switch (e.KeyCode)
+            {
+                case (Keys.Left):
+                    pressedList.Add(pressedKey);
+                    if(pressedList[pressedList.Count() - 2] != pressedKey)
+                    {
+                        BLOCK_MOVE[0] = 0;
+                    }
+                    if (BLOCK_MOVE[0] == 0)
+                    {
+                        mLeft();
+                        ShuffleNumber();
+                        ReturnArray();
+                        RefreshStatus();
+                        Console.Write("{0}, {1}, {2} |", BLOCK_MOVE[0], pressedKey, pressedList[pressedList.Count() - 2]);
+                    }
+                    break;
+
+                case (Keys.Up):
+                    pressedList.Add(pressedKey);
+                    if (pressedList[pressedList.Count() - 2] != pressedKey)
+                    {
+                        BLOCK_MOVE[1] = 0;
+                    }
+                    if (BLOCK_MOVE[1] == 0)
+                    {
+                        mUp();
+                        ShuffleNumber();
+                        ReturnArray();
+                        RefreshStatus();
+                        Console.Write("{0}, {1}, {2} |", BLOCK_MOVE[1], pressedKey, pressedList[pressedList.Count() - 2]);
+                    }
+                    break;
+
+                case (Keys.Right):
+                    pressedList.Add(pressedKey);
+                    if (pressedList[pressedList.Count() - 2] != pressedKey)
+                    {
+                        BLOCK_MOVE[2] = 0;
+                    }
+                    if (BLOCK_MOVE[2] == 0)
+                    {
+                        mRight();
+                        ShuffleNumber();
+                        ReturnArray();
+                        RefreshStatus();
+                        Console.Write("{0}, {1}, {2} |", BLOCK_MOVE[2], pressedKey, pressedList[pressedList.Count() - 2]);
+                    }
+                    break;
+
+                case (Keys.Down):
+                    pressedList.Add(pressedKey);
+                    if (pressedList[pressedList.Count() - 2] != pressedKey)
+                    {
+                        BLOCK_MOVE[3] = 0;
+                    }
+                    if (BLOCK_MOVE[3] == 0)
+                    {
+                        mDown();
+                        ShuffleNumber();
+                        ReturnArray();
+                        RefreshStatus();
+                        Console.Write("{0}, {1}, {2} |", BLOCK_MOVE[3], pressedKey, pressedList[pressedList.Count() - 2]);
+                    }
+                    break;
+
+                default:
+                    break;
+
+            }
+        }
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn(
+            int nLeftRect, // x-coordinate of upper-left corner 
+            int nTopRect, // y-coordinate of upper-left corner 
+            int nRightRect, // x-coordinate of lower-right corner 
+            int nBottomRect, // y-coordinate of lower-right corner 
+            int nWidthEllipse, // height of ellipse 
+            int nHeightEllipse // width of ellipse 
+        );
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+        [System.Runtime.InteropServices.DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+        private void label2_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void pictureBox1_MouseHover(object sender, EventArgs e)
+        {
+            this.pictureBox1.Image = ((System.Drawing.Image)(Properties.Resources.iconhover1));
+        }
+        private void pictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+            this.pictureBox1.Image = ((System.Drawing.Image)(Properties.Resources.icon));
+        }
+    }
+}
